37d400baf44ecafac82aec0dbc10e8fa99dddc7f
Alucherdi <hjgo127942@gmail.com>
2022-08-27 21:32:00 -0500

    Init commit

---
+++ c1.js
@@ -0,0 +1,12 @@
+// Definición de variables
+let nombre = "Guillermo"
+let apellidos = "Herrera Junco"
+
+let nombreCompleto = nombre + " " + apellidos
+
+let edad
+
+edad = (30 - 20) * 2
+
+console.log(nombreCompleto)
+console.log("Edad: " + edad)
