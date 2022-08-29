9913a0e0f4d2c141652e8ecadbca3f0138e6e826
Alucherdi <hjgo127942@gmail.com>
2022-08-28 21:14:19 -0500

    Se agrega clase de expresiones booleanas y condiciones

---
+++ c2.js
@@ -0,0 +1,36 @@
+let numero = 50
+let esMenor50 = numero < 50 && numero > 0
+/*
+
+> es mayor que
+< es menor que
+== es igual que 
+!= es diferente que
+>= es mayor o igual que 
+<= es menor o igual que 
+
+&& conector Y (and)       ampersant
+|| conector O (or)        pip
+
+!(exp bool) negar expresión
+
+*/
+
+
+if (!esMenor50) {
+    console.log("Numero menor a 50")
+} else {
+    console.log("Número mayor a 50")
+}
+
+
+
+
+
+
+/*
+{
+    a = false
+    console.log("Dentro del nivel 2")
+}
+*/
