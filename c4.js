ff651bbb99643b64cb0e9a8a7ee38688e0264dda
Alucherdi <hjgo127942@gmail.com>
2022-09-03 22:35:07 -0500

    Clase de arreglos

---
+++ c4.js
@@ -0,0 +1,21 @@
+let arreglo = [1, 2, 3, 4, 7, 3, 1, 2, 3]
+/*
+Agregar un nuevo valor al FINAL del arreglo
+arreglo.push(elemento)
+
+Elimina el ÚLTIMO valor del arreglo
+arreglo.pop()
+
+Obtener un valor del arreglo
+arreglo[indice]
+
+Si el indice se sale del rango de datos del arreglo
+el valor que resulta es undefined
+
+Obtener el tamaño de un arreglo
+arreglo.length
+*/
+
+for (let i = 0; i < arreglo.length; i++) {
+    console.log("El índice " + i + " es: " + arreglo[i])
+}
